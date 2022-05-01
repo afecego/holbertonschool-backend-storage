a0d88b6cd5471f7335311cec7822161528288bf4
afecego <afecego@gmail.com>
2022-04-30 19:42:52 -0500

    Create a procedure

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- Write a SQL script that creates a stored procedure AddBonus that
+-- adds a new correction for a student.
+
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INTEGER, IN project_name VARCHAR(255), IN score INTEGER)
+BEGIN
+INSERT INTO projects (name)
+SELECT project_name
+WHERE project_name NOT IN (SELECT name FROM projects);
+INSERT INTO corrections (user_id, project_id, score)
+VALUES (user_id, (SELECT id FROM projects WHERE name=project_name), score);
+END$$
+DELIMITER ;
