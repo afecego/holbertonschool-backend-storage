ccd580bae1d4ee0c1cb7687ef098cd3ee3377769
afecego <afecego@gmail.com>
2022-05-04 16:44:50 -0500

    get data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Create a Cache class. In the __init__ method
 """
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union, Optional
 
 
 class Cache:
@@ -18,3 +18,20 @@ class Cache:
         key = str(uuid.uuid1())
         self._redis.mset({key: data})
         return (key)
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """take a key string argument and an optional Callable argument
+        named fn. callable will be used to convert the data back to the desired
+        format"""
+        desire = self._redis.get(key)
+        if fn is not None:
+            return fn(desire)
+        return desire
+
+    def get_str(self, data: str):
+        """parametrize Cache.get with the correct conversion function."""
+        return self.get(key, str)
+
+    def get_int(self, data: int):
+        """parametrize Cache.get with the correct conversion function."""
+        return self.get(key, int)
