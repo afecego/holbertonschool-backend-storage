955914e9a97222a4bd78a6d2f8b5485fd725029d
afecego <afecego@gmail.com>
2022-05-06 23:37:56 -0500

    Commands of Mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
+"""provides some stats about Nginx logs stored in MongoDB
 """
-"""
+
+
--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""lists all documents in a collection:
 """
-"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """Return an empty list if no document in the collection"""
+    return mongo_collection.find()
